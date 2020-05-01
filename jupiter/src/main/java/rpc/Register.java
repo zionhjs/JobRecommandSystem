8d7dce311b0b66865f63f5d25a3031b0df745e7a
ZionHJS <hjszion@gmail.com>
2020-04-30 20:43:55 -0700

    update

--- jupiter/src/main/java/rpc/Register.java
+++ jupiter/src/main/java/rpc/Register.java
@@ -25,14 +25,6 @@ public class Register extends HttpServlet {
     }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -53,5 +45,4 @@ public class Register extends HttpServlet {
 		connection.close();
 		RpcHelper.writeJsonObject(response, obj);
 	}
-
 }
